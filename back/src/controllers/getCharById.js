ee195597a9f089a47d65bfd52fa102561971fbf4
hernan97carp <hernan97carp@hotmail.com>
2023-04-22 22:27:07 -0300

    commit

--- back/src/controllers/getCharById.js
+++ back/src/controllers/getCharById.js
@@ -1,30 +1,49 @@
-const axios = require('axios') // import axios from 'axios'
-
-
-const getCharById = (res, id) => {
-    axios(`https://rickandmortyapi.com/api/character/${id}`) 
-        .then(response => response.data)
-        .then(data => { let character = {
-            id: data.id,
-            name: data.name,
-            gender: data.gender,
-            origin: data.origin,
-            image : data.image,
-            status : data.status,
-            species: data.species
-        } 
-            res
-            .writeHead(200, {'Content-type': 'application/json'})
-            .end(JSON.stringify(character))
-            })
-        .catch(err =>
-            res
-            .writeHead(500, {"Content-type": "text/plain"})
-            .end(`El personaje con id:${id} no fue encontrado`)
-            )
-        
+const URL = "https://rickandmortyapi.com/api/character/";
+const axios = require("axios");
+function getCharById(req, res) {
+  // localhost:3001/rickyandmorty/onsearch/1
+  const { id } = req.params;
+  axios(URL + id).then(
+    (response) => {
+      const character = {
+        id: response.data.id,
+        name: response.data.name,
+        image: response.data.image,
+        gender: response.data.gender,
+        species: response.data.species,
+      };
+      res.status(200).json(character);
+    },
+    (error) => res.status(500).json(error.message)
+  );
 }
-
 
 module.exports = getCharById;
 
+// const axios = require('axios') // import axios from 'axios'
+
+// const getCharById = (res, id) => {
+//     axios(`https://rickandmortyapi.com/api/character/${id}`)
+//         .then(response => response.data)
+//         .then(data => { let character = {
+//             id: data.id,
+//             name: data.name,
+//             gender: data.gender,
+//             origin: data.origin,
+//             image : data.image,
+//             status : data.status,
+//             species: data.species
+//         }
+//             res
+//             .writeHead(200, {'Content-type': 'application/json'})
+//             .end(JSON.stringify(character))
+//             })
+//         .catch(err =>
+//             res
+//             .writeHead(500, {"Content-type": "text/plain"})
+//             .end(`El personaje con id:${id} no fue encontrado`)
+//             )
+
+// }
+
+// module.exports = getCharById;
--- back/src/controllers/getCharDetail.js
+++ back/src/controllers/getCharDetail.js
@@ -1,30 +1,51 @@
-const axios = require('axios') // import axios from 'axios'
-
-
-const getCharDetail = (res, id) => {
-    axios(`https://rickandmortyapi.com/api/character/${id}`) 
-        .then(response => response.data)
-        .then(data => { let character = {
-            id: data.id,
-            name: data.name,
-            gender: data.gender,
-            origin: data.origin,
-            image : data.image,
-            status : data.status,
-            species: data.species
-        } 
-            res
-            .writeHead(200, {'Content-type': 'application/json'})
-            .end(JSON.stringify(character))
-            })
-        .catch(err =>
-            res
-            .writeHead(500, {"Content-type": "text/plain"})
-            .end(`El personaje con id:${id} no fue encontrado`)
-            )
-        
+const URL = "https://rickandmortyapi.com/api/character/";
+const axios = require("axios");
+function getCharDetail(req, res) {
+  // localhost:3001/rickyandmorty/onsearch/1
+  const { id } = req.params;
+  axios(URL + id).then(
+    (response) => {
+      const character = {
+        id: response.data.id,
+        name: response.data.name,
+        image: response.data.image,
+        gender: response.data.gender,
+        species: response.data.species,
+        status: response.data.status,
+        origin: response.data.origin?.name,
+      };
+      res.status(200).json(character);
+    },
+    (error) => res.status(500).json(error.message)
+  );
 }
-
 
 module.exports = getCharDetail;
 
+// const axios = require('axios') // import axios from 'axios'
+
+// const getCharDetail = (res, id) => {
+//     axios(`https://rickandmortyapi.com/api/character/${id}`)
+//         .then(response => response.data)
+//         .then(data => { let character = {
+//             id: data.id,
+//             name: data.name,
+//             gender: data.gender,
+//             origin: data.origin,
+//             image : data.image,
+//             status : data.status,
+//             species: data.species
+//         }
+//             res
+//             .writeHead(200, {'Content-type': 'application/json'})
+//             .end(JSON.stringify(character))
+//             })
+//         .catch(err =>
+//             res
+//             .writeHead(500, {"Content-type": "text/plain"})
+//             .end(`El personaje con id:${id} no fue encontrado`)
+//             )
+
+// }
+
+// module.exports = getCharDetail;
